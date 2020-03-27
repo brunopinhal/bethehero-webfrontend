c35ec36622b3d15c4637049bcbccebd50c503f4e
Bruno Pinhal <b.seek2@gmail.com>
2020-03-27 18:07:54 -0300

    Routes component defined

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 import './styles.css';
 import heroesImg from '../../assets/heroes.png';
@@ -15,7 +16,7 @@ export default function Login(){
           <h1>Faça seu login</h1>
           <input placeholder="Sua ID"/>
           <button className="button" type="submit">Entrar</button>
-          <a href="/register"><FiLogIn size={16} color="#e02041"/>Não tenho cadastro</a>
+          <Link to="/register"><FiLogIn size={16} color="#e02041"/>Não tenho cadastro</Link>
         </form>
       </section>
       <img src={heroesImg} alt="Heroes"/>
