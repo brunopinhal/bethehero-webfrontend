841000f057a9b370081271dd5341eae7c995f40b
Bruno Pinhal <b.seek2@gmail.com>
2020-03-27 21:52:33 -0300

    Login validation

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+
+import api from '../../services/api';
+
 import './styles.css';
 import heroesImg from '../../assets/heroes.png';
 import logo from '../../assets/logo.svg';
@@ -8,13 +11,35 @@ import logo from '../../assets/logo.svg';
 
 
 export default function Login(){
+  const history = useHistory();
+  const [ id, setId] = useState('');
+
+  async function handlerLogin(e) {
+    e.preventDefault();
+    try {
+      const response = await api.post('sessions', { id });
+      console.log(response.data.name);
+
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', response.data.name);
+
+      history.push('profile');
+    }catch (err) {
+      alert(`Falha no login, tente novamente`);
+    }
+  }
+
   return(
     <div className="login-container">
       <section className="form">
         <img src={logo} alt="Be The Hero"/>
-        <form>
+        <form onSubmit={handlerLogin}>
           <h1>Faça seu login</h1>
-          <input placeholder="Sua ID"/>
+          <input
+            placeholder="Sua ID"
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
           <button className="button" type="submit">Entrar</button>
           <Link className="back-link"to="/register"><FiLogIn size={16} color="#e02041"/>Não tenho cadastro</Link>
         </form>
